f8a1aef8c6ea3fe127bc7a7f506300b578f35adb
AntonyRohithAkash <93250352+AntonyRohithAkash@users.noreply.github.com>
2023-09-21 14:27:20 +0530

    Create variables.tf

---
+++ variables.tf
@@ -0,0 +1,14 @@
+variable "username" {
+  description = "username"
+  default     = ""
+}
+
+variable "password" {
+  description = "password"
+  default     = ""
+}
+
+variable "host" {
+  description = "host"
+  default     = ""
+}
