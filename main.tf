55024690f086ad4b1ce31fbd9f291f540a794e37
AntonyRohithAkash <93250352+AntonyRohithAkash@users.noreply.github.com>
2023-09-21 14:29:53 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -3,3 +3,15 @@ provider "ultradns" {
   password = var.password
   hosturl = var.host
 }
+
+resource "ultradns_record" "a" {
+  zone_name = "0-0-0-0-0antony.com."
+  owner_name = "wwww"
+  record_type = "A"
+  ttl = 800
+  record_data = ["1.1.1.1"]
+}
+
+output "Test" {
+  value = ultradns_record.a
+}
