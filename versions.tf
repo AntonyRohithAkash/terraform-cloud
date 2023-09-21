2df829c015f8a7bfcb4f5c542cb3d3085365a421
AntonyRohithAkash <93250352+AntonyRohithAkash@users.noreply.github.com>
2023-09-21 14:24:28 +0530

    Update versions.tf

--- versions.tf
+++ versions.tf
@@ -1,15 +1,8 @@
 terraform {
   required_providers {
-    aws = {
-      source  = "hashicorp/aws"
-      version = "~> 3.28.0"
-    }
-
-    random = {
-      source  = "hashicorp/random"
-      version = "3.0.0"
+    ultradns = {
+      source = "ultradns/ultradns"
+      version = "1.8.0"
     }
   }
-
-  required_version = ">= 0.14.0"
 }
