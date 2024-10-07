d95ebb3280c4de8ff698d62934f33c9140bb0778
AntonyRohithAkash <93250352+AntonyRohithAkash@users.noreply.github.com>
2024-10-07 22:59:03 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -4,15 +4,31 @@ provider "ultradns" {
   hosturl = var.host
 }
 
-resource "ultradns_record" "a" {
-  for_each = toset( ["a", "aa", "aaa", "aaaa","aaaaa"] )
-  zone_name = "0-0-0-0-0antony.com."
-  owner_name = each.key
-  record_type = "A"
-  ttl = 800
-  record_data = ["1.1.1.1"]
+resource "ultradns_zone" "primary" {
+    name        = "2-cst-13592.com."
+    account_name = "GTV8"
+    type        = "PRIMARY"
+    primary_create_info {
+        create_type = "NEW"
+    }
 }
 
-output "Test" {
-  value = ultradns_record.a
+
+resource "ultradns_record" "txt" {
+    count = length(range(1, 1000))
+    zone_name = ultradns_zone.primary.name
+    owner_name = count.index
+    record_type = "TXT"
+    ttl = 500
+    record_data = ["Sample data"]
+}
+
+
+resource "ultradns_record" "A" {
+    count = length(range(1, 1000))
+    zone_name = ultradns_zone.primary.name
+    owner_name = count.index
+    record_type = "A"
+    ttl = 500
+    record_data = ["127.0.0.1"]
 }
